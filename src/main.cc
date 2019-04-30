fd9275db5ae7159ecba7bedef8f87b27ea5080ea
xiao.chen <xiao.chen@mobvoi.com>
2019-04-30 14:46:37 +0800

    ...

--- src/main.cc
+++ src/main.cc
@@ -11,37 +11,18 @@
 using namespace shaw;
 
 int main(int argc,char* argv[]) {
-  // Linkedlist<int> linkedlist;
-  // Node<int>* n1 = linkedlist.addnode(1);
-  // Node<int>* n2 = linkedlist.addnode(2);
-  // Node<int>* n3 = linkedlist.addnode(3);
-  // Node<int>* n4 = linkedlist.addnode(4);
-  // n1->next = n2;
-  // n2->next = n3;
-  // n3->next = n4;
-  // n4->next = nullptr;
-  // Node<int>* head = n1;
-  // while (head != nullptr) {
-  // 	std::cout<<"head="<<head<<", value="<<head->value<<std::endl;
-  // 	head = head->next;
-  // }
-  // Node<int>* new_head = linkedlist.deletenode(n1, 3);
-  // std::cout<<"after delete"<<std::endl;
-  // while (new_head != nullptr) {
-  // 	std::cout<<"head="<<new_head<<", value="<<new_head->value<<std::endl;
-  // 	new_head = new_head->next;
-  // }
   Tree<int> tree;
-  TreeNode<int>* t1 = tree.add_treenode(10);
+  TreeNode<int>* t1 = tree.add_treenode(30);
   TreeNode<int>* t2 = tree.add_treenode(20);
-  TreeNode<int>* t3 = tree.add_treenode(30);
-  TreeNode<int>* t4 = tree.add_treenode(40);
+  TreeNode<int>* t3 = tree.add_treenode(40);
+  TreeNode<int>* t4 = tree.add_treenode(10);
   t1->left = t2;
   t1->right = t3;
   t2->left = t4;
   tree.print(t1);
-  TreeNode<int>* newtree = tree.reverse(t1);
-  std::cout<<"after reverse"<<std::endl;
-  tree.print(newtree);
+  std::cout<<"after insert"<<std::endl;
+  TreeNode<int>* t5 = tree.add_treenode(20);
+  TreeNode<int>* new_head = tree.insert(t1, t5);
+  tree.print(t1);
   return 0;
 }
--- src/tree.h
+++ src/tree.h
@@ -17,7 +17,8 @@ class Tree {
     TreeNode<T>* add_treenode(T t);
     void print(TreeNode<T>* root);
     TreeNode<T>* reverse(TreeNode<T>* root);
-    TreeNode<T>* insert(TreeNode<T>* insert_node);
+    TreeNode<T>* insert(TreeNode<T>* root, TreeNode<T>* insert_node);
+    TreeNode<T>* deletenode(TreeNode<T>* root, TreeNode<T>* delete_node);
 };
 
 template<class T>
@@ -47,15 +48,47 @@ TreeNode<T>* Tree<T>::reverse(TreeNode<T>* root) {
   return root;
 }
 
-// what the fucking code
+
 template<class T>
-TreeNode<T>* Tree::insert(TreeNode<T>* root, TreeNode<T>* insert_node) {
+TreeNode<T>* Tree<T>::insert(TreeNode<T>* root, TreeNode<T>* insert_node) {
   if (root == nullptr) {return insert_node;}
   if (insert_node->value < root->value) {
-  	insert_node->left = root;
-  	return insert_node;
+  	if (root->left == nullptr) {
+      root->left = insert_node;
+      return root;
+    } else {
+      insert(root->left, insert_node);
+    }
+  } else if (insert_node->value > root->value) {
+  	if (root->right == nullptr) {
+  	  root->right = insert_node;
+  	  return root;
+  	} else {
+  	  insert(root->right, insert_node);
+  	}
+  } else {
+  	TreeNode<T>* tmp = root->left;
+  	root->left = insert_node;
+  	insert_node->left = tmp;
+  	return root;
   }
-
 }
+
+//what the fucking code
+template<class T>
+TreeNode<T>* Tree<T>::deletenode(TreeNode<T>* root, TreeNode<T>* delete_node) {
+  if (root == nullptr) {
+  	return root;
+  }
+  if (delete_node->value < root->value) {
+  	deletenode(root->left, delete_node);
+  } else if (delete_node->value > root->value) {
+  	deletenode(root->right, delete_node);
+  } else {
+    // how to record the prev node, fuck.
+  }
+}
+
+
 } //namespace shaw
 #endif //SHAW_TREE_H_
